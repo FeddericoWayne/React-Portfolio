aee39d6d272150570b50edcddca165486f1dbbcc
FeddericoWayne <jackiew1120@hotmail.com>
2023-04-01 16:14:46 -0400

    added to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,10 @@ export default function Footer() {
 
         <div className="footer-wrapper">
             <h5 className="footer-title">Check out my Social Media Accounts + GitHub:</h5>
-            
+            <a href="https://github.com/FeddericoWayne" target="_blank" className="social-icon"><img src='./assets/logos/github-logo.png' id="github" alt="github-link"></img></a>
+            <a href="https://www.linkedin.com/in/fred-diaz-wang-88392313/" target="_blank" className="social-icon"><img src='./assets/logos/linkedin-logo.png' id="linkedin" alt="linkedin-link"></img></a>
+            <a href="https://www.instagram.com/fedderico.wang/?hl=en" target="_blank" className="social-icon"><img src='./assets/logos/instagram-logo.png' id="instagram" alt="instagram-link"></img></a>
+            <a href="https://www.facebook.com/freddie.wampson" target="_blank" className="social-icon"><img src='./assets/logos/facebook-logo.png' id="facebook" alt="facebook-link"></img></a>
         </div>
     )
 
