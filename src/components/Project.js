922dc911550957149c0b33db907a9c6a19982d7a
FeddericoWayne <jackiew1120@hotmail.com>
2023-04-03 15:58:57 -0400

    css fix for navlinks

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,36 +1,72 @@
 import React from 'react';
 import '../styles/project.css'
 
-export default function Project( props ) {
+export default function Project(props) {
 
     if (props.currentView === 'about') {
         return (
             <div className='project-container'>
-                <div className='about-me-container'>
+                <div className='about-me-container in-view'>
                     <div className='about-me'>About Me</div>    
+                </div>
+                <div className='portfolio-container'>
+                    <div className='portfolio'>Portfolio</div>    
+                </div>
+                <div className='contact-container'>
+                    <div className='contact'>Contact</div>    
+                </div>
+                <div className='resume-container'>
+                    <div className='resume'>Resume</div>    
                 </div>
             </div>
         )
     } else if (props.currentView === 'portfolio') {
         return (
             <div className='project-container'>
-                <div className='portfolio-container'>
+                <div className='about-me-container'>
+                    <div className='about-me'>About Me</div>    
+                </div>
+                <div className='portfolio-container in-view'>
                     <div className='portfolio'>Portfolio</div>    
+                </div>
+                <div className='contact-container'>
+                    <div className='contact'>Contact</div>    
+                </div>
+                <div className='resume-container'>
+                    <div className='resume'>Resume</div>    
                 </div>
             </div>
         )
     } else if (props.currentView === 'contact') {
         return (
             <div className='project-container'>
-                <div className='contact-container'>
+                <div className='about-me-container'>
+                    <div className='about-me'>About Me</div>    
+                </div>
+                <div className='portfolio-container'>
+                    <div className='portfolio'>Portfolio</div>    
+                </div>
+                <div className='contact-container in-view'>
                     <div className='contact'>Contact</div>    
+                </div>
+                <div className='resume-container'>
+                    <div className='resume'>Resume</div>    
                 </div>
             </div>
         )
     } else {
         return (
             <div className='project-container'>
-                <div className='resume-container'>
+                <div className='about-me-container'>
+                    <div className='about-me'>About Me</div>    
+                </div>
+                <div className='portfolio-container'>
+                    <div className='portfolio'>Portfolio</div>    
+                </div>
+                <div className='contact-container'>
+                    <div className='contact'>Contact</div>    
+                </div>
+                <div className='resume-container in-view'>
                     <div className='resume'>Resume</div>    
                 </div>
             </div>
