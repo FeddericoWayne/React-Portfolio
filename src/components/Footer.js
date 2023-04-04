d284d6b3f64d7ecdee609e36547cb5128aad38a7
FeddericoWayne <jackiew1120@hotmail.com>
2023-04-03 22:16:24 -0400

    fixed typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
 
         <div className="footer-container">
-            <h5 className="footer-title">Check out my GitHub + Social Media Accounts:</h5>
+            <h5 className="footer-title">Check out my GitHub & Social Media Accounts:</h5>
             <a href="https://github.com/FeddericoWayne" target="_blank" className="social-icon"><img src='./assets/logos/github-logo.png' id="github" alt="github-link"></img></a>
             <a href="https://www.linkedin.com/in/fred-diaz-wang-88392313/" target="_blank" className="social-icon"><img src='./assets/logos/linkedin-logo.png' id="linkedin" alt="linkedin-link"></img></a>
             <a href="https://www.instagram.com/fedderico.wang/?hl=en" target="_blank" className="social-icon"><img src='./assets/logos/instagram-logo.png' id="instagram" alt="instagram-link"></img></a>
