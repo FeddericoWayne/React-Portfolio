d1af5301fec3655e8cc62770359c6aaf60dfcc2b
FeddericoWayne <jackiew1120@hotmail.com>
2023-04-04 09:45:32 -0400

    added animation to highlighted navlink

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,16 +8,16 @@ export default function Naviation({ currentView,handlePageDisplay }) {
         <div className='navlinks-container'>
 
             <div className='navlinks'>
-                <a href="#about" onClick={()=>handlePageDisplay('about')} className={currentView === 'about'? 'navlink active' : 'navlink'}>About Me</a>
+                <a href="#about" onClick={()=>handlePageDisplay('about')} className={currentView === 'about'? 'navlink active-right' : 'navlink'}>About Me</a>
             </div>
             <div className='navlinks'>
-                <a href="#portfolio" onClick={()=>handlePageDisplay('portfolio')} className={currentView === 'portfolio'? 'navlink active' : 'navlink'}>Portfolio</a>
+                <a href="#portfolio" onClick={()=>handlePageDisplay('portfolio')} className={currentView === 'portfolio'? 'navlink active-left' : 'navlink'}>Portfolio</a>
             </div>
             <div className='navlinks'>
-                <a href="#contact" onClick={()=>handlePageDisplay('contact')} className={currentView === 'contact'? 'navlink active' : 'navlink'}>Contact</a>
+                <a href="#contact" onClick={()=>handlePageDisplay('contact')} className={currentView === 'contact'? 'navlink active-right' : 'navlink'}>Contact</a>
             </div>
             <div className='navlinks'>
-                <a href="#resume" onClick={()=>handlePageDisplay('resume')} className={currentView === 'resume'? 'navlink active' : 'navlink'}>Resume</a>
+                <a href="#resume" onClick={()=>handlePageDisplay('resume')} className={currentView === 'resume'? 'navlink active-left' : 'navlink'}>Resume</a>
             </div>
 
         </div>
