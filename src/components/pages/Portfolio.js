7de1ecd9ada58fb73dd19ce2671b74a2719c4484
FeddericoWayne <jackiew1120@hotmail.com>
2023-04-05 22:55:09 -0400

    added horizontal rule and back to top button to project page

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -2,13 +2,29 @@ import React from 'react';
 import '../../styles/pages.css';
 
 
-
 export default function Portfolio(props) {
+
+    const projectLength = props.projects.length;
 
     return (
         <div className='portfolio-content'>
             <h4 id='portfolio-title'>Previous Projects:</h4>
-            {props.projects.map((project)=>{
+            {props.projects.map((project, index)=>{
+
+                if (index === projectLength -1) {
+                    return (
+                        <div key={project.title} className='app-container'>
+                            <h3 className='app-title'>{project.title}</h3>
+                            <img className='screenshot' src={project.src} alt={project.title}></img>
+                            <div className='link-container'>
+                                <a href={project.appUrl} target='_blank' className='app-link'>Go to Application</a>
+                                <a href={project.repo} target='_blank' className='app-link'>GitHub Repo</a>
+                            </div>
+                            <a href='#portfolio-title' id='back'>Back to Top</a>
+                        </div>
+                    )
+                }
+                
                 return (
                     <div key={project.title} className='app-container'>
                         <h3 className='app-title'>{project.title}</h3>
@@ -17,6 +33,7 @@ export default function Portfolio(props) {
                             <a href={project.appUrl} target='_blank' className='app-link'>Go to Application</a>
                             <a href={project.repo} target='_blank' className='app-link'>GitHub Repo</a>
                         </div>
+                        <hr className='partition'/>
                     </div>
                 )
             })}
