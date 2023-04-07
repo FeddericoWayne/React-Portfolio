a37c99640da27116db812afbeb12ac96d14462db
FeddericoWayne <jackiew1120@hotmail.com>
2023-04-07 10:54:16 -0400

    added codes to Contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,16 +3,18 @@ import '../../styles/pages.css';
 
 export default function Contact() {
 
-    const [ nameWarning, setNameWarning ] = useState(false);
-    const [ emailWarning, setEmailWarning ] = useState(false);
-    const [ messageWarning, setMessageWarning ] = useState(false);
+    const [ nameWarning, setNameWarning ] = useState(undefined);
+    const [ emailWarning, setEmailWarning ] = useState(undefined);
+    const [ messageWarning, setMessageWarning ] = useState(undefined);
+    const [ submitWarning, setSubmitWarning ] = useState(undefined);
 
     const checkName = (e) => {
 
         if (e.target.value === '') {
-            setNameWarning(true);
+            setNameWarning("Oops! Please enter your name!");
+            setSubmitWarning("");
         } else {
-            setNameWarning(false);
+            setNameWarning("");
         }
 
     };
@@ -22,65 +24,92 @@ export default function Contact() {
         const regEx = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ ;
 
         if (e.target.value.match(regEx)) {
-            setEmailWarning(false);
+            setEmailWarning("");
         } else if (e.target.value === '') {
-            setEmailWarning(true);
+            setEmailWarning("Oops! Please enter a valid email!");
+            setSubmitWarning("");
         } else {
-            setEmailWarning(true);
+            setEmailWarning("Oops! Please enter a valid email!");
+            setSubmitWarning("");
         }
     };
 
     const checkMessage = (e) => {
 
         if (e.target.value === '') {
-            setMessageWarning(true);
+            setMessageWarning("Oops! Please enter your message!");
+            setSubmitWarning("");
         } else {
-            setMessageWarning(false);
+            setMessageWarning("");
         }
-    }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (nameWarning === undefined || emailWarning === undefined || messageWarning === undefined) {
+            
+            document.getElementById("submit-warning").classList.remove("green");
+            setSubmitWarning("Please enter all the required fields!");
+            setTimeout(()=> {
+                setSubmitWarning("");
+            },1500);
+            return;
+        };
+
+        if (nameWarning.length || emailWarning.length || messageWarning.length) {
+
+            document.getElementById("submit-warning").classList.remove("green");
+            setSubmitWarning("Please enter all the required fields!");
+            setTimeout(()=> {
+                setSubmitWarning("");
+            },1500);
+            return;
+        };
+
+        if (!nameWarning.length && !emailWarning.length && !messageWarning.length) {
+
+            document.getElementById("submit-warning").classList.add("green");
+            setSubmitWarning("Message submitted. Thank you!")
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            setTimeout(()=> {
+                setSubmitWarning("");
+                document.location.reload();
+            },2000);
+            
+        }
+    };
 
 
     return (
         <div className='contact-content'>
             <h4 id='contact-title'>Contact Me:</h4>
                 <div className='form-container'>
-                    {nameWarning ? (
-                        <div className='input-container'>
-                            <label className='label'>Name:</label>
-                            <input onBlur={checkName} id="name" type="text" placeholder='Enter Your Name Here ...'></input>
-                            <p className='warning'>Oops! Please enter your name!</p>
-                        </div>
-                    ) : (
-                        <div className='input-container'>
-                            <label className='label'>Name:</label>
-                            <input onBlur={checkName} id="name" type="text" placeholder='Enter Your Name Here ...'></input>
-                        </div>
-                    )}
-                    {emailWarning ? (
-                        <div className='input-container'>
-                            <label className='label'>Email:</label>
-                            <input onBlur={checkEmail} id="email" type='text' placeholder='Enter Your Email Here ...'></input>
-                            <p className='warning'>Oops! Please enter a valid email!</p>
-                        </div>
-                    ) : (
-                        <div className='input-container'>
-                            <label className='label'>Email:</label>
-                            <input onBlur={checkEmail} id="email" type='text' placeholder='Enter Your Email Here ...'></input>
-                        </div>
-                    )}
-                    {messageWarning ? (
-                        <div className='message-container'>
-                            <label>Message:</label>
-                            <textarea onBlur={checkMessage} id="message" placeholder='Enter Your Message Here ...'></textarea>
-                            <p className='warning'>Oops! Please enter your message!</p>
-                        </div>
-                    ) : (
-                        <div className='message-container'>
-                            <label>Message:</label>
-                            <textarea onBlur={checkMessage} id="message" placeholder='Enter Your Message Here ...'></textarea>  
-                        </div>
-                    )}
-                    <button type='submit' id='submit'>Submit</button>
+
+                    <div className='input-container'>
+                        <label className='label'>Name:</label>
+                        <input onChange={checkName} onBlur ={checkName} id="name" type="text" placeholder='Enter Your Name Here ...'></input>
+                        <p className='warning'>{nameWarning}</p>
+                    </div>
+
+                    <div className='input-container'>
+                        <label className='label'>Email:</label>
+                        <input onChange={checkEmail} onBlur={checkEmail} id="email" type='text' placeholder='Enter Your Email Here ...'></input>
+                        <p className='warning'>{emailWarning}</p>
+                    </div>
+
+                    <div className='message-container'>
+                        <label>Message:</label>
+                        <textarea onChange={checkMessage} onBlur={checkMessage} id="message" placeholder='Enter Your Message Here ...'></textarea>
+                        <p className='warning'>{messageWarning}</p>
+                    </div>
+
+                    <div>
+                        <button onClick={handleSubmit} type='submit' id='submit'>Submit</button>
+                        <p className='submit-warning' id='submit-warning'>{submitWarning}</p>
+                    </div>
                 </div>
 
             <div className='contact-info-container'>
